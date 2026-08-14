0bd85061846d7a04650de33142d27245ab23b2c9
Vivek Yadav <vivek.y@ebrandz.com>
2026-08-14 13:33:19 +0530

    Initial Cypress automation framework setup

---
+++ cypress/pages/login/LoginPage.js
@@ -0,0 +1,26 @@
+class LoginPage {
+
+    visit() {
+        cy.visit('https://qa-practice.netlify.app/auth_ecommerce.html');
+    }
+
+    enterEmail(email) {
+        cy.get('#email').type(email);
+    }
+
+    enterPassword(password) {
+        cy.get('#password').type(password);
+    }
+
+    clickLogin() {
+        cy.get('#submitLoginBtn').click();
+    }
+
+    login(email, password) {
+        this.enterEmail(email);
+        this.enterPassword(password);
+        this.clickLogin();
+    }
+}
+
+export default new LoginPage();
